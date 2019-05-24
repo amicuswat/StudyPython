8ec001c2c3391f1d6352f0fbe51918a8b9760e79
pythonwizard <amicuswat@gmail.com>
2019-05-24 10:42:12 +0400

    Dungeon Game in command line and Dunder __main__ best practice.

---
+++ TreeHouse/PyBasic/dunder.py
@@ -0,0 +1,7 @@
+def print_hello():
+    print("Hello from dunder.")
+
+print(__name__)
+
+if __name__ == "__main__":
+    print_hello()
---
+++ TreeHouse/PyBasic/second_dunder.py
@@ -0,0 +1,4 @@
+import dunder
+
+print("Hello from second_dunder.")
+dunder.print_hello()
