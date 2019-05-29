08730f049165448831da1d96c6ffa383c92e9736
pythonwizard <amicuswat@gmail.com>
2019-05-29 08:14:41 +0400

    OOPy just a bunch more of inheritance and some other things

--- TreeHouse/PyBasic/OO_Python/circle.py
+++ TreeHouse/PyBasic/OO_Python/circle.py
@@ -6,6 +6,12 @@ class Circle:
     def radius(self):
         return self.diameter / 2
 
+    @radius.setter
+    def radius(self, radius):
+        self.diameter = radius * 2
+
 small = Circle(10)
 print(small.diameter)
 print(small.radius)
+small.radius = 10
+print(small.diameter)
