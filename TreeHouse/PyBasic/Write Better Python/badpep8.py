ab6e1b362113398db852e2cffc3c02b0e304b75f
pythonwizard <amicuswat@gmail.com>
2019-05-29 10:52:29 +0400

    two lines between classes and other items

--- TreeHouse/PyBasic/Write Better Python/badpep8.py
+++ TreeHouse/PyBasic/Write Better Python/badpep8.py
@@ -3,10 +3,11 @@ def fooBar(arg1,arg2,arg3,arg4):
          # way too much indentation
          return arg1,arg2,arg3,arg4
 
-         
+
 def bar(*args):
     # bad spacing
     return 2+2
+
 
 # Bad class name, bad spacing, bad indentation
 class treehouse:
