d3b16e617c18bf7aa07356e9fea62f43a18eda9b
pythonwizard <amicuswat@gmail.com>
2019-05-29 11:14:11 +0400

    functions and variables names should be lower case with _ between words. classes should start with Capital letter every word

--- TreeHouse/PyBasic/Write Better Python/badpep8.py
+++ TreeHouse/PyBasic/Write Better Python/badpep8.py
@@ -2,7 +2,7 @@ import sys
 import random
 
 # multiple imports
-def fooBar(arg1, arg2, arg3, arg4):
+def foo_bar(arg1, arg2, arg3, arg4):
     # way too much indentation
     return arg1, arg2, arg3, arg4
 
@@ -13,7 +13,7 @@ def bar(*args):
 
 
 # Bad class name, bad spacing, bad indentation
-class treehouse:
+class Treehouse:
     def one(self):
         return 1
     def two(self):
@@ -21,7 +21,7 @@ class treehouse:
 
 
 # bad identation and whitespace
-alpha, betta, charlie, delta = fooBar("a long string",
+alpha, betta, charlie, delta = foo_bar("a long string",
                   "a longer string",
                   "yet another long string",
                   "and other crazy string")
