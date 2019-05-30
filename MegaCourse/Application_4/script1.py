9a87213888fdc8b2f2834d3786f828523a25f5f1
pythonwizard <amicuswat@gmail.com>
2019-05-30 19:07:10 +0400

    added explanations concerning the meaning of the lines - now things are getting more clear

--- MegaCourse/Application_4/script1.py
+++ MegaCourse/Application_4/script1.py
@@ -1,10 +1,17 @@
 from flask import Flask
 
+#__name__ is a special variable that receives the name of python script
+# if we import this script the __name__ will be script1
+# but if we execute this script the __name__ = __main__
 app = Flask(__name__)
 
+# @app.route('/') - with this we will find the page by localhost:5000
+# if we change '/' to '/about/' the site will be accessed at localhost:5000/about
 @app.route('/')
 def home():
     return "Website content goes here!"
 
+# bellow lines will not run if we import the script as the __name__ will be "script1"
+# they run only if we execute this script so the __name__ will be "__main__"
 if __name__ == "__main__":
     app.run(debug=True)
