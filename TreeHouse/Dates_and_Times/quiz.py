3e82afde80038ab8183dae0d442a890911fb6b00
pythonwizard <amicuswat@gmail.com>
2019-06-03 13:48:50 +0400

    Added questions generation, total correct answers count and summery

--- TreeHouse/Dates_and_Times/quiz.py
+++ TreeHouse/Dates_and_Times/quiz.py
@@ -9,7 +9,13 @@ class Quiz:
     answers = []
 
     def __init__(self):
+        question_types = (Add, Multiply)
         # generat 10 random questions with numbers from 1 to 10
+        for _ in range(10):
+            num1 = random.randint(1, 10)
+            num2 = random.randint(1, 10)
+            question = random.choice(question_types)(num1, num2)
+            self.questions.append(question)
         # add these questions into self.questions
 
     def take_quiz(self):
@@ -18,6 +24,7 @@ class Quiz:
         # log if they got question right
         # log the end time
         # show a summary
+        pass
 
     def ask(self, question):
         # log the start time
@@ -27,7 +34,22 @@ class Quiz:
         # if the answer's right, send back True
         # otherwise, send back False
         # send back the elapsed time, too
+        pass
+
+    def total_correct(self):
+        # return the total # of correct answers
+        total = 0
+        for answers in self.answers:
+            if answer[0]:
+                total += 1
+        return total
 
     def summary(self):
         # print how many you got right and the total # of questions. 5/10
-        # print the total time for the quiz: 30 seconds!  
+        print("You got {} out of {} right.".format(
+            self.total_correct(), len(self.quesitons)
+        ))
+        # print the total time for the quiz: 30 seconds!
+        print("It took you {} seconds total.".format(
+            (self.end_time-self.start_time).seconds
+        ))
