21965511a490943ab006a0a5398785a9eacf4508
pythonwizard <amicuswat@gmail.com>
2019-06-13 09:51:48 +0400

    Added printing functionality using some key word

--- TreeHouse/InterPy/File_IO/remember.py
+++ TreeHouse/InterPy/File_IO/remember.py
@@ -12,13 +12,14 @@ def rememberer(thing):
 
 def show():
     #open file
-    #print out each line in the file
-    #close file
-    pass
+    with open('database.txt') as file:
+        for line in file:
+            print(line)
+    
 
 if __name__ == '__main__':
     if sys.argv[1].lower() == '--list':
         show()
     else:
         rememberer(' '.join(sys.argv[1:]))
-    rememberer(input("What should I remember > "))
+    #rememberer(input("What should I remember > "))
