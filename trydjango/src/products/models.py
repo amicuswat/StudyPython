677ed80e537baad6350cd8290d1acdace7d25d74
pythonwizard <amicuswat@gmail.com>
2019-06-17 22:46:00 +0400

    Changed issues in model. added other 000 files just to see changes in them(they are changed automatically) db added by mistake in previous commit - will keep it, yet not sure what will happen if i delete it. it should be restored I believe but loose the data.

--- trydjango/src/products/models.py
+++ trydjango/src/products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-	title = models.TextField
-	description = models.TextField
-	price = models.TextField
+	title = models.TextField(default="Product name")
+	description = models.TextField(default="Product description")
+	price = models.TextField(default="0")
+	summary = models.TextField(default="This is cool!")
