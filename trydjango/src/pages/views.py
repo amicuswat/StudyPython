31bf1243b357249a97add5bd8050ef25c409990e
pythonwizard <amicuswat@gmail.com>
2019-06-18 17:58:26 +0400

    Rendered data from base to html.

--- trydjango/src/pages/views.py
+++ trydjango/src/pages/views.py
@@ -13,7 +13,8 @@ def about_view(request, *args, **kwargs):
 	my_context = {
 		"title": "this page is about me",
 		"my_number": 123,
-		"my_list": [123, 321, 4242, 12313, "abc"]
+		"my_list": [123, 321, 4242, 12313, "abc"],
+		"my_html": "<h1> Hello world </h1>"
 	}
 	return render(request, "about.html", my_context)
 
--- trydjango/src/trydjango/urls.py
+++ trydjango/src/trydjango/urls.py
@@ -20,11 +20,12 @@ from pages.views import home_view
 from pages.views import contacts_view
 from pages.views import about_view
 from pages.views import social_view
+from products.views import product_detail_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('contacts/', contacts_view, name='contacts'),
     path('about/', about_view, name='about'),
-    path('social/', social_view, name='social'),
+    path('product/', product_detail_view, name='product'),
     path('admin/', admin.site.urls),
 ]
