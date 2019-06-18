0aab3a023912963b42feb3e136cd300e79152555
pythonwizard <amicuswat@gmail.com>
2019-06-18 13:26:41 +0400

    Added one model

--- trydjango/src/products/models.py
+++ trydjango/src/products/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-	title = models.TextField(default="Product name")
-	description = models.TextField(default="Product description")
-	price = models.TextField(default="0")
-	summary = models.TextField(default="This is cool!")
+	title = models.CharField(max_length=120)
+	description = models.TextField(blank=True, null=True)
+	price = models.DecimalField(decimal_places=2, max_digits=100)
+	summary = models.TextField(blank=False, null=False)
+	featured = models.BooleanField()
