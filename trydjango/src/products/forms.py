cbd06828e80bce4b88c619497363ee9256371f39
pythonwizard <amicuswat@gmail.com>
2019-06-22 00:27:55 +0400

    A form, I added attributes to fields.And changed the view functions so that I can now commumicate with data in back end or save it to DB

--- trydjango/src/products/forms.py
+++ trydjango/src/products/forms.py
@@ -10,3 +10,20 @@ class ProductForm(forms.ModelForm):
 			'description',
 			'price'
 		]
+
+class RawProductForm(forms.Form):
+	title = forms.CharField(widget=forms.TextInput(attrs={
+		"placeholder": "Product title"
+	}))
+	description = forms.CharField(required=False,
+		widget=forms.Textarea(
+			attrs={
+				"placeholder": "Input product description",
+				"class": "new-class-name",
+				"id": "my-id-for-textarea",
+				"rows": 20,
+				"cols": 120
+			}
+		)
+	)
+	price = forms.DecimalField(initial=19.99)
