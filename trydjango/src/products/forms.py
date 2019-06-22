6b919c5bba8a01f9249d6536104e112ff17bc765
pythonwizard <amicuswat@gmail.com>
2019-06-22 14:16:54 +0400

    Custom validation for some fields

--- trydjango/src/products/forms.py
+++ trydjango/src/products/forms.py
@@ -3,6 +3,21 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+	title = forms.CharField(widget=forms.TextInput(attrs={
+		"placeholder": "Product title"
+	}))
+	description = forms.CharField(required=False,
+		widget=forms.Textarea(
+			attrs={
+				"placeholder": "Input product description",
+				"class": "new-class-name",
+				"id": "my-id-for-textarea",
+				"rows": 20,
+				"cols": 120
+			}
+		)
+	)
+	price = forms.DecimalField(initial=19.99)
 	class Meta:
 		model = Product
 		fields = [
@@ -10,6 +25,12 @@ class ProductForm(forms.ModelForm):
 			'description',
 			'price'
 		]
+	def clean_title(self, *args, **kwargs):
+		title = self.cleaned_data.get("title")
+		if not "CFE" in title:
+			raise forms.ValidationError("This is not a valid title")
+		return title
+
 
 class RawProductForm(forms.Form):
 	title = forms.CharField(widget=forms.TextInput(attrs={
